3cd8ebb5fd361a667d2a466f65f3c75de9ba4dbc
DerPumu <derpumu@users.noreply.github.com>
2025-06-19 16:23:09 +0200

    add locale and subgroups for ammo-category

--- prototype/ammo-category.lua
+++ prototype/ammo-category.lua
@@ -3,51 +3,63 @@ local ammo_categories =
 {
   {
     type = "ammo-category",
-    name = "rocket-mk2"
+    name = "rocket-mk2",
+    subgroup = "ammo-category",
   },
   {
     type = "ammo-category",
-    name = "rocket-mk3"
+    name = "rocket-mk3",
+    subgroup = "ammo-category",
   },
   {
     type = "ammo-category",
-    name = "bullet-mk2"
+    name = "bullet-mk2",
+    subgroup = "ammo-category",
   },
   {
     type = "ammo-category",
-    name = "sniper-round"
+    name = "sniper-round",
+    subgroup = "ammo-category",
   },
   {
     type = "ammo-category",
-    name = "shotgun-shells-mk2"
+    name = "shotgun-shells-mk2",
+    subgroup = "ammo-category",
   },
   {
     type = "ammo-category",
-    name = "shotgun-shells-mk3"
+    name = "shotgun-shells-mk3",
+    subgroup = "ammo-category",
   },
   {
     type = "ammo-category",
-    name = "shotgun-shells-mk4"
+    name = "shotgun-shells-mk4",
+    subgroup = "ammo-category",
   },
   {
     type = "ammo-category",
-    name = "railgun-mk2-round"
+    name = "railgun-mk2-round",
+    subgroup = "ammo-category",
   },
   {
     type = "ammo-category",
-    name = "black-holes"
+    name = "black-holes",
+    subgroup = "ammo-category",
   },
   {
     type = "ammo-category",
-    name = "end-game"
+    name = "end-game",
+    subgroup = "ammo-category",
   },
   {
     type = "ammo-category",
-    name = "end-game-2"
+    name = "end-game-2",
+    subgroup = "ammo-category",
   },
   {
     type = "ammo-category",
-    name = "healing"
+    name = "healing",
+    subgroup = "ammo-category",
   },
 }
 
